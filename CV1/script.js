1a8d2b7bf9909e00d1f896e08ed128b7b8aad402
Petar31 <petarpirot.pp@gmail.com>
2018-04-17 12:21:42 +0200

    HTML, CSS, Bootstrap, JS

---
+++ CV1/script.js
@@ -0,0 +1,11 @@
+/*var btnGroup = document.getElementsByClassName('btn-group');
+function showBtnGroup(){
+    btnGroup[0].style.visibility = "visible";
+}
+window.addEventListener('load',function(){
+    setTimeout(showBtnGroup, 500);
+})*/
+
+$(document).ready(function(){
+    $("#btnGroup").show(1500);
+})
